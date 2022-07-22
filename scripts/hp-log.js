000ef90a35660cc2f322419268f0c5698b38fa03
Henning Koehler <henning.koehler.nz@gmail.com>
2022-07-23 11:48:01 +1200

    track nonlethal damage as well

--- scripts/hp-log.js
+++ scripts/hp-log.js
@@ -1,18 +1,25 @@
 Hooks.on('preUpdateActor', (actor, data, options, user_id) => {
     let newHP = data.data.attributes.hp.value;
     let newTemp = data.data.attributes.hp.temp;
+    let newNL = data.data.attributes.hp.nonlethal;
     // only create message when hp are updated
-    if (newHP !== undefined || newTemp !== undefined) {
+    if (newHP !== undefined || newTemp !== undefined || newNL !== undefined) {
         let oldHP = actor.data.data.attributes.hp.value;
         let oldTemp = actor.data.data.attributes.hp.temp;
+        let oldNL = actor.data.data.attributes.hp.nonlethal;
         // makes sure new values are defined
         if (newHP === undefined)
             newHP = oldHP;
         if (newTemp === undefined)
             newTemp = oldTemp;
+        if (newNL === undefined)
+            newNL = oldNL;
         // create message
-        var hpFormat = function(hp, temp) { return temp ? `${hp}+${temp}` : hp; };
-        let msg = `hp: ${hpFormat(oldHP, oldTemp)} -> ${hpFormat(newHP, newTemp)}`;
+        var hpFormat = function(hp, temp, nl) {
+            let full = temp ? `${hp}+${temp}` : hp;
+            return nl ? `${full}-${nl}` : full;
+        };
+        let msg = `hp: ${hpFormat(oldHP, oldTemp, oldNL)} -> ${hpFormat(newHP, newTemp, newNL)}`;
         // show skulls if dead
         if (newHP <= -actor.data.data.abilities.con.total) {
             let img = '<img src="icons/svg/skull.svg" width="20" height="20" style="vertical-align: middle;border-style: none;margin-left: 20px;margin-right:20px">';
